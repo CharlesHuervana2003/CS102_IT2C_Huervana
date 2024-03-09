54562cb579f4e4bb05d46667f6d5f46d124d8792
Charles Huervana <charlesbjarne.huervana@students.isatu.edu.ph>
2024-03-09 09:11:11 +0000

    LAB3 COMMIT

--- Lab Exercise 3/Creating Database.R
+++ Lab Exercise 3/Creating Database.R
@@ -17,10 +17,10 @@
 # CREATE TABLE reviews (
   # id INT PRIMARY KEY AUTO_INCREMENT,
   # Title VARCHAR(50),
-  # Reviewer VARCHAR(50),
+  # Reviewer VARCHAR(100),
   # ReviewDate VARCHAR(50),
-  # Rating VARCHAR(50),
-  # Review VARCHAR(50),
+  # Rating VARCHAR(10),
+  # Review VARCHAR(500),
   # ReviewTitle VARCHAR(100),
   # );
 )
