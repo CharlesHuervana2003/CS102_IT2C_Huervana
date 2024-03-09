c679b806e369bcfcf704ba2b4cfb53a0a87216ed
Charles Huervana <charlesbjarne.huervana@students.isatu.edu.ph>
2024-03-09 08:45:52 +0000

    LAB3 COMMIT

---
+++ Lab Exercise 3/Creating Database.R
@@ -0,0 +1,31 @@
+#Procedures
+
+
+|||||||||||||||||||||||||||||||
+  
+#ACCESING DATABASE:
+  
+  
+# cd  "E:\xampp\mysql\bin"
+# mysql -u root -p
+# CREATE DATABASE huervanalab3;
+# USE huervanalab3;
+||||||||||||||||||||||||||||||||
+  
+#CREATING TABLE:
+  
+# CREATE TABLE reviews (
+  # id INT PRIMARY KEY AUTO_INCREMENT,
+  # Title VARCHAR(50),
+  # Reviewer VARCHAR(50),
+  # ReviewDate VARCHAR(50),
+  # Rating VARCHAR(50),
+  # Review VARCHAR(50),
+  # ReviewTitle VARCHAR(100),
+  # );
+)
+
+|||||||||||||||||||||||||||||||||
+
+  
+  
