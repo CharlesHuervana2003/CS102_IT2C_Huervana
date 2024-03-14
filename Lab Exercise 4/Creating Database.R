2cabd5d82896ddbaf2c81f84ca2057945ca99a64
Charles Huervana <charlesbjarne.huervana@students.isatu.edu.ph>
2024-03-14 02:12:27 +0000

    LAB4 COMMIT

---
+++ Lab Exercise 4/Creating Database.R
@@ -0,0 +1,27 @@
+#Procedures
+
+
+|||||||||||||||||||||||||||||||
+  
+  #ACCESING DATABASE:
+  
+  
+  # cd  "E:\xampp\mysql\bin"
+  # mysql -u root -p
+  # CREATE DATABASE huervanalab4;
+  # USE huervanalab4;
+  ||||||||||||||||||||||||||||||||
+  
+  #CREATING TABLE:
+  
+  # CREATE TABLE webscrape (
+  # id INT PRIMARY KEY AUTO_INCREMENT,
+  # title VARCHAR(500),
+  # author VARCHAR(500),
+  # subject VARCHAR(100),
+  # abstract VARCHAR(500),
+  # meta VARCHAR(150),
+# );
+)
+
+|||||||||||||||||||||||||||||||||
