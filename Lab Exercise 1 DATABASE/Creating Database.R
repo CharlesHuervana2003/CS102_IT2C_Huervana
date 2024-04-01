724fdaa4cc17cc0ede3b4f81870e6623cff5b90d
Charles Huervana <charlesbjarne.huervana@students.isatu.edu.ph>
2024-04-01 12:48:14 +0000

    LAB1 DATABASE COMMIT

---
+++ Lab Exercise 1 DATABASE/Creating Database.R
@@ -0,0 +1,31 @@
+#Procedures
+
+
+|||||||||||||||||||||||||||||||
+  
+  #ACCESING DATABASE:
+  
+  
+  # cd  "E:\xampp\mysql\bin"
+  # mysql -u root -p
+  # CREATE DATABASE huervanalab1DB;
+  # USE huervanalab1DB;
+  ||||||||||||||||||||||||||||||||
+  
+  #CREATING TABLE:
+  
+  #dbWriteTable(connection, 'lab1Monitor', monitor, append = TRUE)
+  #dbWriteTable(connection, 'lab1Mouse', mouse, append = TRUE)
+  #dbWriteTable(connection, 'lab1tenProd', tenProd, append = TRUE))
+  
+  |||||||||||||||||||||||||||||||||
+  
+  #RESULT
+  #SELECT * FROM lab1Monitor;
+  #SELECT * FROM lab1Mouse;
+  #SELECT * FROM lab1tenProd;
+  
+  #100 rows in set (0.000 sec)
+  #100 rows in set (0.000 sec)
+  #250 rows in set (0.001 sec)
+  
