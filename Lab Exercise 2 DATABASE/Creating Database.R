f94c6ad50b5611113d666c0c3ffe40ebeb0fc6be
Charles Huervana <charlesbjarne.huervana@students.isatu.edu.ph>
2024-04-01 12:20:07 +0000

    LAB2 DATABASE COMMIT

---
+++ Lab Exercise 2 DATABASE/Creating Database.R
@@ -0,0 +1,25 @@
+#Procedures
+
+
+|||||||||||||||||||||||||||||||
+  
+  #ACCESING DATABASE:
+  
+  
+  # cd  "E:\xampp\mysql\bin"
+  # mysql -u root -p
+  # CREATE DATABASE huervanalab2DB;
+  # USE huervanalab3;
+  ||||||||||||||||||||||||||||||||
+  
+  #CREATING TABLE:
+  
+  #dbWriteTable(connection, 'lab2reviews', allproducts, append = TRUE)
+
+|||||||||||||||||||||||||||||||||
+  
+  #RESULT
+  #SELECT * FROM lab2reviews;
+  #2500 rows in set (0.006 sec)
+  
+  
